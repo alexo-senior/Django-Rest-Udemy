fb281c68a1f118086fb8240254e31d9f9fe56e9a
alexo-senior <112290994+alexo-senior@users.noreply.github.com>
2025-03-06 14:34:12 -0500

    se instala la libreria mysqlclient, ademas de requirements.txt

--- backend/urls.py
+++ backend/urls.py
@@ -3,8 +3,9 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
-
-
+#esta importacion es para poder subir archivos en el navegador
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -12,4 +13,13 @@ urlpatterns = [
     path('', include('home.urls')),
     path('api/v1/',include('ejemplo.urls')),
 ]
+
+#estos son los archivos que se encuentran en el settings.py, 
+# los estamos importando para que se puedan subir archivos
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
+
+
 
