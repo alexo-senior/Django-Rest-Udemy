b76eb7f4f07e18f2c6d74c6056a5391b755b9e03
alexo-senior <112290994+alexo-senior@users.noreply.github.com>
2025-03-26 17:50:42 -0500

    se crea la funcion post con manejo de errores, y la funcion put con slugify

--- categorias/urls.py
+++ categorias/urls.py
@@ -4,6 +4,6 @@ from .views import *
 #se crea la ruta en la url de la app categoria
 urlpatterns = [
     path('categorias',Clase1.as_view()),
-    path('categorias/<int:id>',Clase2.as_view())
+    path('categorias/<int:id>',Clase2.as_view()),
     
 ]
--- categorias/views.py
+++ categorias/views.py
@@ -6,6 +6,9 @@ from . models import Categoria
 from . serializers import CategoriaSerializer
 from http import HTTPStatus
 from django.http import Http404
+from django.utils.text import slugify
+
+
 
 
 
@@ -18,26 +21,74 @@ class Clase1(APIView):
         data = Categoria.objects.order_by('-id').all()
         data_json = CategoriaSerializer(data, many=True)
         #return Response(data_json.data)
-        #la forma correcta es usa json_Response por seguridad de los datos
         #return JsonResponse(data_json.data)
         #se puede envolver en un objeto llamado data para que se vea mas ordenado
         #y de esa forma se visualiza en la web para probar que los datos se estan mostrando correctamente
         return JsonResponse({"data": data_json.data}, status=HTTPStatus.OK)
         #se puede hacer una respuesta tipo response para probar que los datos se estan mostrando correctamente
         #return Response(data)
+        
+        #SE CREA LA FUNCION PARA EL METODO POST
+    def post(self, request):
+        #validacion para que el campo nombre no este vacio
+        if request.data.get("nombre")==None or not request.data['nombre']:
+            return JsonResponse({"estado":"error", "mensaje":"el campo nombre no puesde estar vacio"},
+                                status= HTTPStatus.BAD_REQUEST)
+        try:
+        # Se crea un nuevo registro en la tabla Categoria solo enviando el nombre
+            Categoria.objects.create(nombre=request.data['nombre'])
+            # Se retorna una respuesta JsonResponse con un mensaje de éxito y un estado 201
+            return JsonResponse({"estado": "ok", "mensaje": "se crea el registro exitosamente"}, status=HTTPStatus.CREATED)
+        except Exception as e:
+            raise Http404
+        
+                
+            
+                
+        
 
 #listar un registro por su id
 class Clase2(APIView):
     def get(self, request, id):
         try:
         #se puede usar tambien pk al consultar id o llave primaria
-        #se usa get en vez de all() para que solo traiga los registros pedidos
+        #se usa get en vez de all() para que solo traiga los registros pedidos sin corchetes
             data = Categoria.objects.filter(id=id).get()
             return JsonResponse({'data':{'id':data.id,'nombre':data.nombre, 'slug':data.slug}}, status=HTTPStatus.OK)
         #se le agrega el manejo de errores try except, el DoesnotExist es el 
         #error que parece cuando no se encuentra el registro, por lo tanto lo puedes copiar
         except Categoria.DoesnotExist:
             raise Http404
+        
+        
+    def put(self, request, id):
+        if request.data.get("nombre")==None:
+            return JsonResponse({"estado":"error", "mensaje":"el campo nombre no puesde estar vacio"},
+            status= HTTPStatus.BAD_REQUEST)
+            
+        if not request.data.get("nombre"):
+            return JsonResponse({"estado":"error", "mensaje":"el campo nombre no puesde estar vacio"},
+            status= HTTPStatus.BAD_REQUEST)
+            #se recomienda colocar una excepcion
+        try:
+            data = Categoria.objects.filter(id=id).get()#pk es lo mismo que id
+            #se obtiene por filter de la categoria el id y por medio de update 
+            #se actualiza el nombre solamente al que pertenece el id
+            Categoria.objects.filter(id=id).update(nombre=request.data.get('nombre'), 
+            slug=slugify(request.data.get('nombre')))
+            return JsonResponse({"estado": "ok", "mensaje": "se modifica el registro exitosamente"}, 
+            status=HTTPStatus.OK)
+        except Categoria.DoesNotExist:
+            raise Http404
+                
+            
+                
+        
+        
+        
+        
+        
+        
         
     
     
