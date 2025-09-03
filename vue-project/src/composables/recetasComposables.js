ba335d10952c01a83147c336d594ebfa78f0186d
alexis <alexobarrafer@gmail.com>
2025-09-03 13:46:11 -0500

    se crea el codigo para el codigo del composable y la respuesta reactiva

--- vue-project/src/composables/recetasComposables.js
+++ vue-project/src/composables/recetasComposables.js
@@ -8,8 +8,18 @@ export function recetasComposable()
 
   let getDatos = async()=>
   {
+    try{
+        const res = await fetch(`${import.meta.env.VITE_API_URL}recetas`,
+          {headers:{'content-type':'application-json'}});
+          datos.value = await res.json();
+    } catch (err) {
+      error.value=err;
+
+    }
 
   };
+  getDatos();
+
   return{
     datos:readonly(datos),
     error:readonly(error)
